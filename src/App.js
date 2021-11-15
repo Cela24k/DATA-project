06df8faf1ad8c8bb34e64b73d57a63a2f2a3f52d
Cela24k <882778@stud.unive.it>
2021-11-15 17:06:56 +0100

    Aggiunto il corpo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3a1LIWTdxdfPwioq0tbsHlQxc2DIx23E",
+  authDomain: "mysite-5b666.firebaseapp.com",
+  projectId: "mysite-5b666",
+  storageBucket: "mysite-5b666.appspot.com",
+  messagingSenderId: "629817619071",
+  appId: "1:629817619071:web:f2cd83999084816eb7854a"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
   return (
     <div className="App">
+      <div className="La-mimmo-Arte">
+        Ciao Mimmo buonasera! canagliaa
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work in progress.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/alessandroceladon_/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cliccami
         </a>
       </header>
     </div>
