923baec3239df1613a92eb5b158776c81deb0bc5
Cela24k <882778@stud.unive.it>
2021-11-17 02:53:06 +0100

    Setup ambiente 3d

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app"
+
+import React from "react"
+import "./App.css"
+import Modello from "./Components/Modello"; 
+import ThreeScene from "./Components/ThreeScene"
+/*
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useSpring, animated } from 'react-spring'
+import Main from "./Components/Main"
+
+*/
 
 const firebaseConfig = {
   apiKey: "AIzaSyA3a1LIWTdxdfPwioq0tbsHlQxc2DIx23E",
@@ -16,25 +25,21 @@ const app = initializeApp(firebaseConfig);
 function App() {
   return (
     <div className="App">
-      <div className="La-mimmo-Arte">
-        Ciao Mimmo buonasera! canagliaa
-      </div>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Work in progress.
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/alessandroceladon_/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Cliccami
-        </a>
-      </header>
+      <div style={floatingText}>Ciao.</div> 
+      <ThreeScene/>
     </div>
+    
   );
 }
 
+const floatingText = {
+  color: '#f3f3f3',
+  position:'absolute',
+  'zIndex': 1,
+  left: '120px',
+  top: '200px',
+  width:'100px', 
+  height:'20px',
+  fontSize:'600%'
+}
 export default App;
